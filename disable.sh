889b94bf6d275fbd6b41c1db5560cc35d9741347
tyler williams <tyler@aos.codes>
2025-10-07 18:43:12 -0400

    typo

--- disable.sh
+++ disable.sh
@@ -3,7 +3,7 @@
 sudo systemctl daemon-reload
 
 sudo systemctl disable camera-folder-watcher
-sudo systemctl diable camera-watcher
+sudo systemctl disable camera-watcher
 
 sudo systemctl stop camera-folder-watcher
 sudo systemctl stop camera-watcher
