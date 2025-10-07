e9b53a59b9ac9ddfc94f20960d6735150b992926
tyler williams <tyler@aos.codes>
2025-10-07 17:56:17 -0400

    correct folders

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 npm i
 
 cp camera-folder-watcher.service /lib/systemd/system/camera-folder-watcher.service
-cp camera-watcher.service /lib/systemd/system/camera--watcher.service
+cp camera-watcher.service /lib/systemd/system/camera-watcher.service
 
 sudo systemctl daemon-reload
 sudo systemctl start camera-folder-watcher
